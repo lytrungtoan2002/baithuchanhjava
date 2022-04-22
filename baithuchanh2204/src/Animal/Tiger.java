970bd6e93ee29b0e8a14a0ae776bbb39387b8de0
Toàn Trung Lý <lytoan874@gmail.com>
2022-04-22 14:34:00 +0700

    commit note

---
+++ baithuchanh2204/src/Animal/Tiger.java
@@ -0,0 +1,23 @@
+package Animal;
+public class Tiger extends Animal {
+    void eat() {
+        System.out.println("Ho an thit");
+    }
+
+    void sleep() {
+        System.out.println("Ho di ngu");
+    }
+
+    void walk() {
+        System.out.println("Ho di bang 4 chan");
+    }
+
+    void run() {
+        System.out.println("Ho chay rat nhanh");
+    }
+
+    void roar() {
+        System.out.print("Ho gao rat to");
+    }
+}
+© 2022 GitHub, Inc.
